5161e3500cb953104afb3b53009cba4a2ced1131
Sebastian Montero <sebastian.montero@gmail.com>
2024-06-19 18:44:26 -0600

    Developed the eslint config file. Developed the script to support the specification of various resource requests. Developed script to print the cleos power up command for each account.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const {Resources, PowerUpState} = require('@wharfkit/resources');
-const {APIClient} = require('@wharfkit/antelope');
-
-
+const {Resources} = require('@wharfkit/resources');
 
 
 async function main() {
+  let requests = [
+    {account: 'nftmarb.beny', ms: 20, mb: 2},
+    {account: 'rex1.beny', ms: 20, mb: 3}
+  ];
+  
+
   const resources = new Resources({
     url: 'https://eos.greymass.com',
     sampleAccount: 'dapp.beny',
@@ -17,19 +20,61 @@ async function main() {
   const sample = await resources.getSampledUsage()
   // console.log(sample.account.account_name.toString(), sample.cpu.toNumber(), sample.net.toNumber())
 
-  const ms = 50
-  const us = ms*1000
-  const kb = 1024*1
-  const bytes = kb*1024
+  // const ms = 20
+  // const us = ms*1000
+  // const kb = 1024*2
+  // const bytes = kb*1024
 
-  // const cpuWeight = powerup.cpu.us_to_weight(sample.cpu, us)
-  // const netWeight = powerup.net.bytes_to_weight(sample.net, bytes)
-  const cpuFrac = powerup.cpu.frac_by_us(sample, us)
-  const netFrac = powerup.net.frac_by_bytes(sample, bytes)
-  const cpuPrice = powerup.cpu.price_per_ms(sample, ms)
-  const netPrice = powerup.net.price_per_kb(sample, kb)
-  console.log(`ms: ${ms}, kb: ${kb}`)
-  console.log(`netFrac: ${netFrac}, cpuFrac: ${cpuFrac}, cpuPrice: ${cpuPrice}, netPrice: ${netPrice}, total price: ${cpuPrice+netPrice}`)
+  requests = requests.map(({account, ms, us, mb, kb, b}) => {
+    if(!mb && !kb && !b){
+      throw new Error('must specify one of mb, kb, b')
+    }
+    if (!us && !ms){
+      throw new Error('must specify one of us, ms')
+    }
+    if(kb){
+      b = kb*1024
+    } else if(mb){
+      b = mb*1024*1024
+    }
+    if(ms){
+      us = ms*1000
+    }
+    const cpuFrac = powerup.cpu.frac_by_us(sample, us)
+    const netFrac = powerup.net.frac_by_bytes(sample, b)
+    const cpuPrice = powerup.cpu.price_per_us(sample, us)
+    const netPrice = powerup.net.price_per_byte(sample, b)
+    const totalPrice = (cpuPrice+netPrice).toFixed(4)
+    ms = us/1000
+    kb = b/1024
+    mb = kb/1024
+    console.log(`account: ${account}, us: ${us}, b: ${b}, cpuFrac: ${cpuFrac}, netFrac: ${netFrac}, totalPrice: ${totalPrice}, ms: ${ms}, kb: ${kb}, mb: ${mb}`)
+    return {
+      account,
+      us,
+      b,
+      cpuFrac,
+      netFrac,
+      cpuPrice,
+      netPrice,
+      totalPrice
+    }
+  })
+  console.log("")
+  requests.forEach(async ({account, cpuFrac, netFrac, totalPrice}) => {
+    console.log(`cleos --url=https://eos.greymass.com push action eosio powerup '["dapp.beny", "${account}", "1", "${netFrac}", "${cpuFrac}", "${totalPrice} EOS"]' -p dapp.beny`)
+  })
+
+  // // const cpuWeight = powerup.cpu.us_to_weight(sample.cpu, us)
+  // // const netWeight = powerup.net.bytes_to_weight(sample.net, bytes)
+  // const cpuFrac = powerup.cpu.frac_by_us(sample, us)
+  // const netFrac = powerup.net.frac_by_bytes(sample, bytes)
+  // const cpuPrice = powerup.cpu.price_per_ms(sample, ms)
+  // const netPrice = powerup.net.price_per_kb(sample, kb)
+  // console.log(`ms: ${ms}, kb: ${kb}`)
+  // const totalPrice = (cpuPrice+netPrice).toFixed(4)
+  // console.log(`netFrac: ${netFrac}, cpuFrac: ${cpuFrac}, cpuPrice: ${cpuPrice}, netPrice: ${netPrice}, total price: ${totalPrice}`)
+  // console.log(`cleos --url=https://eos.greymass.com push action eosio powerup '["dapp.beny", "${account}", "1", "${netFrac}", "${cpuFrac}", "${totalPrice} EOS"]' -p dapp.beny`)
   
 }
 
